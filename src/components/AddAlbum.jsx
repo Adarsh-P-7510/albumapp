02e1f91ee1f2f59666cecfb0292bfb07de661e4a
Adarsh-P-7510 <adarshp7510@gmail.com>
2024-06-07 22:59:35 +0530

    created

---
+++ src/components/AddAlbum.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const AddAlbum = () => {
+  return (
+    <div>
+      <NavBar/>
+        <div className="container">
+          <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              <div className="row">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-12 col-xxl-12">
+                  <label htmlFor="" className="form label">userid</label>
+                  <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-12 col-xxl-12">
+                  <label htmlFor="" className="form label">id</label>
+                  <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-12 col-xxl-12">
+                  <label htmlFor="" className="form label">title</label>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-12 col-xxl-12">
+                  <label htmlFor="" className="form-form">size</label>
+                  <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-12 col-xxl-12">
+                  <label htmlFor="" className="form label">quality</label>
+                  <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-12 col-xxl-12">
+                  <button className="btn btn-success">addphphoto</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddAlbum
