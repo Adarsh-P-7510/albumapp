8cdc86a424965f6a87dc988a83fefec661789bc4
Adarsh-P-7510 <adarshp7510@gmail.com>
2024-06-08 12:31:31 +0530

    api created

--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -19,6 +19,7 @@ const AddAlbum = () => {
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-12 col-xxl-12">
                   <label htmlFor="" className="form label">title</label>
+                  <input type="text" className="form-control" />
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-12 col-xxl-12">
                   <label htmlFor="" className="form-form">size</label>
